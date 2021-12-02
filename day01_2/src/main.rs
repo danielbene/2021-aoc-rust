093a97d0b0f2b2e12b232fc64812a30f3ccc9521
danielbene <danielbene@protonmail.com>
2021-12-02 17:11:12 +0100

    Add solution to part 2

--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let solution = solve(&mut lines_vec);
     let runtime = Instant::now() - start;
 
-    let output = format!("Solution: {}\nRuntime: {}ms ({} microsecs)\n",
+    let output = format!("Solution: {}\nRuntime: {}ms ({} microsec)\n",
         solution, runtime.as_millis(), runtime.as_micros());
     fs::write("solution.txt", output).expect("Unable to write file");
 }
@@ -22,25 +22,11 @@ fn solve(input: &mut Vec<String>) -> i16 {
     let mut prev: i16 = 0;
     let item_cnt = input.len();
 
-    let dif = item_cnt % 3;
-    if dif != 0 {
-        println!("dif is {}", dif);
-        for i in 1..=dif {
-            input.remove(item_cnt - i);
-            println!("removing item in index {}", item_cnt - i);
-        }
-    }
-
-    // TODO: implement sliding
-
-    for triplet in 0..item_cnt/3 {
+    for measurement in 0..item_cnt-2 {
         let mut current: i16 = 0;
         for i in 0..3 {
-            let index = triplet * 3 + i;
-            current += input.get(index).unwrap().parse::<i16>().unwrap();
+            current += input.get(measurement + i).unwrap().parse::<i16>().unwrap();
         }
-
-        println!("prev: {}, curr: {}", prev, current);
 
         if current > prev {
             counter += 1;
@@ -49,15 +35,14 @@ fn solve(input: &mut Vec<String>) -> i16 {
         prev = current;
     }
 
-    println!("counter {}", counter);
-
     return counter;
 }
 
-fn load_from_file(file_path: &str) ->  Vec<String> {
+fn load_from_file(file_path: &str) -> Vec<String> {
     let file = fs::File::open(file_path).expect("Something went wrong reading the file");
     let reader = BufReader::new(file);
 
+    // yep, it's a frickin return, get used to it
     reader.lines()
         .map(|line| line.unwrap().parse::<String>().unwrap())
         .collect()
