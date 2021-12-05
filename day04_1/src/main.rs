1035fdd8f6b4e21f01a9b82d41010daeda9cbdeb
danielbene <danielbene@protonmail.com>
2021-12-05 13:28:25 +0100

    Add solution to part 1

--- day04_1/src/main.rs
+++ day04_1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     aoc_util::end(solution as isize, init_tuple.1);
 }
 
-// welp, it is what it is
+// welp, this is NOT nice
 fn solve(input: &mut Vec<String>) -> isize{
     let mut table_stats: Vec<(u8, i8, u16)> = Vec::new();
     let mut nums: Vec<u8> = Vec::new();
@@ -15,12 +15,14 @@ fn solve(input: &mut Vec<String>) -> isize{
     }
 
     for (t_index, t_line_index) in (2..input.len()).step_by(6).enumerate() {
-        let mut lowest_weight: i8 = nums.len() as i8;
+        let max_weight = nums.len();
+        let mut lowest_weight: i8 = max_weight as i8;
+        let mut column_weights = vec!(0i8; 5);
 
         for i in t_line_index..t_line_index + 5 {
             // test lines
             let mut highest_index: i8 = 0;
-
+            let mut col_i = 0;  // cant use enumerate bc double spaces
             for line_num in input.get(i).unwrap().split(" ") {
                 if line_num == "" {
                     continue
@@ -32,18 +34,29 @@ fn solve(input: &mut Vec<String>) -> isize{
                     if h_idx > highest_index {
                         highest_index = h_idx;
                     }
+
+                    if *column_weights.get(col_i).unwrap() != -1i8 && h_idx > *column_weights.get(col_i).unwrap() {
+                        column_weights[col_i] = h_idx;
+                    }
                 } else {
                     highest_index = -1;
+                    column_weights[col_i] = -1;
                     break
                 }
+
+                col_i += 1;
             }
 
             if highest_index != -1 && highest_index < lowest_weight {
                 lowest_weight = highest_index;
             }
+        }
 
-            // test columns
-            // TODO: implement matrix column check
+        // compare table column weights
+        for col_w in column_weights.iter() {
+            if *col_w < lowest_weight {
+                lowest_weight = *col_w;
+            }
         }
 
         let mut table_sum: u16 = 0;
@@ -64,9 +77,6 @@ fn solve(input: &mut Vec<String>) -> isize{
     }
 
     table_stats.sort_by_key(|k| k.1);
-
-    //println!("{:?}", nums);
-    //println!("{:?}", table_stats);
 
     let winner = table_stats.get(0).unwrap();
     *nums.get(winner.1 as usize).unwrap() as isize * winner.2 as isize
