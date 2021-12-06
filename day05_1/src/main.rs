79c28cb079b81c1771b895d3ce34d8852fbcf734
danielbene <danielbene@protonmail.com>
2021-12-06 15:32:01 +0100

    Add solution to part 1

---
+++ day05_1/src/main.rs
@@ -0,0 +1,67 @@
+use aoc_util;
+
+const MATRIX_SIZE: usize = 999;
+
+fn main() {
+    let mut init_tuple = aoc_util::init();
+    let solution = solve(&mut init_tuple.0);
+    aoc_util::end(solution as isize, init_tuple.1);
+}
+
+fn solve(input: &mut Vec<String>) -> isize{
+    let mut counter: u64 = 0;
+    // 2darray causes stackoverflow on 1k size - use Vec, it is stored on heap...
+    // let mut terrain = [[0u16; MATRIX_SIZE]; MATRIX_SIZE];
+    let mut terrain_vec = vec![vec![0; MATRIX_SIZE]; MATRIX_SIZE];
+    for line in input {
+        let coords: Vec<&str> = line.split(" -> ").collect();
+        let s: Vec<&str> = coords.get(0).unwrap().split(",").collect();
+        let e: Vec<&str> = coords.get(1).unwrap().split(",").collect();
+
+        let start: (u16, u16) = (s[0].parse().unwrap(), s[1].parse().unwrap());
+        let end: (u16, u16) = (e[0].parse().unwrap(), e[1].parse().unwrap());
+        if start.0 != end.0 && start.1 == end.1 {
+            mtx_job_on_x(start, end, &mut terrain_vec, &mut counter);
+        } else if start.0 == end.0 && start.1 != end.1 {
+            mtx_job_on_y(start, end, &mut terrain_vec, &mut counter);
+        }
+    }
+
+    counter as isize
+}
+
+fn mtx_job_on_x(start: (u16, u16), end: (u16, u16), array: &mut Vec<Vec<u16>>, cnt: &mut u64) {
+    let mut range = end.0..=start.0;
+    let fix = start.1;
+    if start.0 < end.0 {
+        range = start.0..=end.0;
+    }
+
+    for i in range {
+        array[fix as usize][i as usize] += 1;
+        if array[fix as usize][i as usize] == 2 {
+            *cnt += 1;
+        }
+    }
+}
+
+fn mtx_job_on_y(start: (u16, u16), end: (u16, u16), array: &mut Vec<Vec<u16>>, cnt: &mut u64) {
+    let mut range = end.1..=start.1;
+    let fix = start.0;
+    if start.1 < end.1 {
+        range = start.1..=end.1;
+    }
+
+    for i in range {
+        array[i as usize][fix as usize] += 1;
+        if array[i as usize][fix as usize] == 2 {
+            *cnt += 1;
+        }
+    }
+}
+
+#[test]
+fn tests() {
+    let mut vec: Vec<String> = aoc_util::load_from_file("test.txt");
+    assert!(solve(&mut vec) == 5);
+}
